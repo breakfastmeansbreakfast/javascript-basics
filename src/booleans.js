4bd47502dfe2061dd06e92c56eebcc1d2aba0bbd
Cat Hallam <catherinehallam@mutumojaa.com>
2018-06-03 16:56:54 +0100

    Boolean.js updated

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,61 @@
 const negate = (a) => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return (a && b);
 };
 
 const either = (a, b) => {
-  // your code here
+  return (a || b);
 };
 
 const none = (a, b) => {
-  // your code here
+ return (!a && !b);
 };
 
 const one = (a, b) => {
-  // your code here
+  return (!a && b) || (!b && a);
 };
 
 const truthiness = (a) => {
-  // your code here
+  return !!(a);
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return (a === b);
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return (a > b);
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return (a <= b);
 };
 
 const isOdd = (a) => {
-  // your code here
+  return (a % 2 !== 0) ;
 };
 
 const isEven = (a) => {
-  // your code here
+ return (a % 2 === 0);
 };
 
 const isSquare = (a) => {
-  // your code here
+return Math.sqrt(a) % 1 === 0;
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return string.substr(0, 1) === char;
 };
 
 const containsVowels = (string) => {
-  // your code here
+  
 };
 
 const isLowerCase = (string) => {
-  // your code here
+  return string.toLowerCase() === string;
 };
 
 module.exports = {
